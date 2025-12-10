16f809c8bdc6787b04481b81649761ba934d92f9
Ruan de Jager <ruan@etse.co.za>
2025-12-10 15:28:54 +0200

    Battery module success

--- Worker/Battery/Battery_Config.h
+++ Worker/Battery/Battery_Config.h
@@ -21,8 +21,9 @@
 #define BAT_SAMPLE_VALUE_DELTA_MAX	20.
 
 // Might be board-specific
-#define BAT_MEAS_ADC_M_NUM      4300
-#define BAT_MEAS_ADC_M_DEN      1366  // 4096 / 3.0v
-#define BAT_MEAS_ADC_C          0
+#define BAT_MEAS_ADC_M_NUM   7510
+#define BAT_MEAS_ADC_M_DEN   4095
+#define BAT_MEAS_ADC_C       0
+
 
 #endif /* WORKER_BATTERY_BATTERY_CONFIG_H_ */
